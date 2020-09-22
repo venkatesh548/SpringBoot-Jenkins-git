a7b6651af119972b9d2d9067a8f9340f5b8d994f
T <venkatesh.t@capgemini.com>
2020-09-22 22:52:50 +0530

    commit with Logger in Unit Test

--- src/test/java/com/moviecatalog/moviecatalogService/MoviecatalogServiceApplicationTests.java
+++ src/test/java/com/moviecatalog/moviecatalogService/MoviecatalogServiceApplicationTests.java
@@ -2,18 +2,27 @@ package com.moviecatalog.moviecatalogService;
 
 import static org.junit.Assert.assertEquals;
 
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.moviecatalog.moviecatalogService.Controller.MovieCatalogResource;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 class MoviecatalogServiceApplicationTests {
 
+	public static Logger LOGGER = LogManager.getLogger(MoviecatalogServiceApplicationTests.class);
+	
 	@Test
 	void contextLoads() {
 		
+		LOGGER.info("Entred into Test cases .....");
 		assertEquals(true,true);
 	}
 
